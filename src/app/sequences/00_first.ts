00aad56cf54d4467dad85faffe5f26251968b05f
ES-Itoshima <131435520+ES-Itoshima@users.noreply.github.com>
2023-10-31 19:42:54 +0900

    とりあえずグッドニュースまで

---
+++ src/app/sequences/00_first.ts
@@ -0,0 +1,18 @@
+import { Sequence } from '.'
+import { renderPreset } from '..'
+import { UNIVERSE, addrs } from '../consts'
+import { CL } from '../presets/cl'
+import { COLOR_RED } from '../presets/colors'
+import { GROUND_BASE, GROUND_TASUMAKI_CHASE2 } from '../presets/ground'
+import { LED_CHASE_DOROTHY, LED_SINWAVE3 } from '../presets/led'
+
+export const first: Sequence[] = [
+    {
+        note: 'グッドニュース',
+        steps: [
+            {
+                id: 'ニュートラル',
+                position: 60 * 0,
+            },
+        ]
+    }]
