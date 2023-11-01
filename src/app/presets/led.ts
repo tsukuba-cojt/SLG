f488171c57aa79d01ddf60369237177e39c770f5
ES-Itoshima <131435520+ES-Itoshima@users.noreply.github.com>
2023-11-01 15:19:19 +0900

    LED左右で別の管理

--- src/app/presets/led.ts
+++ src/app/presets/led.ts
@@ -2,72 +2,121 @@ import { Preset } from '.'
 import { UNIVERSE as U, addrs } from '../consts'
 import { sin } from '../lib'
 export const 
-LED_ON = 'led.on',
-LED_90 = 'led.90',
-LED_80 = 'led.80',
-LED_75 = 'led.75',
-LED_70 = 'led.70',
-LED_60 = 'led.60',
-LED_30 = 'led.30',
-LED_20 = 'led.20'
+LED_ON_D = 'led.ond',
+LED_90_D = 'led.90d',
+LED_80_D = 'led.80d',
+LED_75_D = 'led.75d',
+LED_70_D = 'led.70d',
+LED_60_D = 'led.60d',
+LED_30_D = 'led.30d',
+LED_20_D = 'led.20d',
+LED_ON_O = 'led.ond',
+LED_90_O = 'led.90d',
+LED_80_O = 'led.80d',
+LED_75_O = 'led.75d',
+LED_70_O = 'led.70d',
+LED_60_O = 'led.60d',
+LED_30_O = 'led.30d',
+LED_20_O = 'led.20d'
+
 
 
 
 
 export const led: Preset[] = [
   {
-    id: LED_ON,
+    id: LED_ON_D,
     render(d, _, s = 255) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_ON_O,
+    render(d, _, s = 255) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_90,
+    id: LED_90_D,
     render(d, _, s = 202.5) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_90_O,
+    render(d, _, s = 202.5) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_80,
+    id: LED_80_D,
     render(d, _, s = 180) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_80_O,
+    render(d, _, s = 180) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_75,
+    id: LED_75_D,
     render(d, _, s = 168.75) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_75_O,
+    render(d, _, s = 168.75) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_70,
+    id: LED_70_D,
     render(d, _, s = 157.5) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_70_O,
+    render(d, _, s = 157.5) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_60,
+    id: LED_60_D,
     render(d, _, s = 135) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_60_O,
+    render(d, _, s = 135) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_30,
+    id: LED_30_D,
     render(d, _, s = 67.5) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_30_O,
+    render(d, _, s = 67.5) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
   {
-    id: LED_20,
+    id: LED_20_D,
     render(d, _, s = 45) {
       d.set(U, addrs.LED_D_DIM, s)
+    },
+  },
+  {
+    id: LED_20_O,
+    render(d, _, s = 45) {
       d.set(U, addrs.LED_O_DIM, s)
     },
   },
