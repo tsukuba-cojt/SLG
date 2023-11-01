16a6d5fd16c899dfd9952b280f63f6cd3caf86b3
ES-Itoshima <131435520+ES-Itoshima@users.noreply.github.com>
2023-11-01 14:11:20 +0900

    魔法使いと私

--- src/app/sequences/00_first.ts
+++ src/app/sequences/00_first.ts
@@ -1,8 +1,7 @@
 import { Sequence } from '.'
-import { renderPreset } from '..'
-import { UNIVERSE, addrs } from '../consts'
-import { COLOR_RED } from '../presets/colors'
+import { COLOR_BLUE_D, COLOR_GREENYELLOW_D, COLOR_GREEN_D, COLOR_GREEN_O, COLOR_LIGHTBLUE_O, COLOR_WHITE_D, COLOR_WHITE_O } from '../presets/colors'
 import { LED_ON } from '../presets/led'
+
 
 export const first: Sequence[] = [
     {
@@ -19,8 +18,53 @@ export const first: Sequence[] = [
         note: '魔法使いと私',
         steps: [
             {
-                id: 'ニュートラル',
+                id: '「ああ、ミス・エルファバ！/」',
                 position: 60 * 0,
+                fade: 3.5,
+                extends:[
+                    LED_ON,
+                    COLOR_WHITE_D,
+                    COLOR_WHITE_O
+                ]
+            },
+            {
+                id: '「受け取るわ 贈り物/」',
+                position: 60 * 2,
+                fade: 3.41,
+                extends:[
+                    LED_ON,
+                    COLOR_GREEN_O,
+                    COLOR_GREEN_D
+                ]
+            },
+            {
+                id: '「自分の力 信じるわ/」～「彼は言うのよ/」',
+                position: 60 * 4,
+                fade: 4.5,
+                extends:[
+                    LED_ON,
+                    COLOR_GREENYELLOW_D,
+                    COLOR_WHITE_O
+                ]
+            },
+            {
+                id: '「生まれ変わる 私が/」',
+                position: 60 * 6,
+                fade: 4.0,
+                extends:[
+                    LED_ON,
+                    COLOR_BLUE_D,
+                    COLOR_LIGHTBLUE_O
+                ]
+            },
+            {
+                id: '「その日が/ 来た」',
+                position: 60 * 7,
+                extends:[
+                    LED_ON,
+                    COLOR_GREEN_D,
+                    COLOR_WHITE_O
+                ]
             },
         ],
         
