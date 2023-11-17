d56bfa5f1907a2af86e6cdfea2d69d899c297a07
ES-Itoshima <131435520+ES-Itoshima@users.noreply.github.com>
2023-11-17 17:20:36 +0900

    機能追加

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,100 +1,180 @@
 <template lang="pug">
 div.wrap
-  div.left
-    div.input-group-wrap
-      div.input-group(v-for="i in 400")
-        div.label {{ i }}
-        circular-input(:value="$store.getters.values[i - 1] || 0" :max="255" @input="$store.dispatch('overrideFrame', [i, $event])")
-    div.presets
-      button(@click="$store.dispatch('clearOverrides')") Clear Overrides
-      span.preset-selector(
-        v-for="preset in presets"
-        @click="togglePreset(preset.id)"
-        :class="{'is-active': overridePresets.includes(preset.id)}"
-      ) {{ preset.id }}
-  sequence-row.right
+    div.left
+      div.input-group-wrap
+        div.input-group(v-for="i in 400")
+          div.label {{ i }}
+          circular-input(:value="$store.getters.values[i - 1] || 0" :max="255" @input="$store.dispatch('overrideFrame', [i, $event])")
+      div.presets
+        button(@click="$store.dispatch('clearOverrides')") Clear Overrides
+        span.preset-selector(
+          v-for="preset in presets"
+          @click="togglePreset(preset.id)"
+          :class="{'is-active': overridePresets.includes(preset.id)}"
+        ) {{ preset.id }}
+    sequence-row.sequence
+    div.right
+      div.container
+        input(type="text" placeholder="脚本を入力")
+        button 照明プランを生成
+        button 再生成
+        input(type="text" placeholder="選択肢1")
+        input(type="text" placeholder="選択肢2")
+        input(type="text" placeholder="選択肢3")
+        p
+        input(type="text" placeholder="フェード")
+        input(type="text" placeholder="サイクル")
+        button 次のセクションへ
+        button(@click="addressSetUp") アドレス設定
+        button アドレス削除
+        button アドレス一覧
+        div.popup(v-if="isPopupVisible", :style="{ top: popupPosition.y + 'px', left: popupPosition.x + 'px' }", @mousedown="startDrag")
+            div.popup-content
+              h2 アドレス設定
+              input(type="text" placeholder="name")
+              input(type="text" placeholder="Address")
+              input(type="text" placeholder="number")
+              p
+              button 登録
+              button(@click="endSetUp") 終了
 </template>
-
-<script>
-import { CircularInput } from '@nandenjin/alien-ui'
-import SequenceRow from './components/SequenceRow.vue'
-export default {
-  name: 'TsukuliveLightingCommander',
-  components: {
-    CircularInput,
-    SequenceRow,
-  },
-  computed: {
-    presets() {
-      return this.$store.state.presets.presets
+  
+  <script>
+  import { CircularInput } from '@nandenjin/alien-ui'
+  import SequenceRow from './components/SequenceRow.vue'
+  export default {
+    name:'TsukuliveLightingCommander',
+    components: {
+      CircularInput,
+      SequenceRow
     },
-    overridePresets() {
-      return this.$store.state.presets.overridePresets
+    data() {
+      return {
+        isPopupVisible: false,
+        popupPosition: { x: 0, y: 0 },
+        dragData: { x: 0, y: 0 }
+      };
     },
-  },
-  mounted() {
-    window.addEventListener('keydown', e => {
-      if (e.key === ' ') {
-        this.$store.dispatch('tap')
-        e.preventDefault()
-      }
-    })
-  },
-  methods: {
-    togglePreset(id) {
-      const overrides = this.$store.state.presets.overridePresets
-      if (!overrides.includes(id)) {
-        this.$store.dispatch('updateOverridePresets', [...overrides, id])
-      } else {
-        this.$store.dispatch(
-          'updateOverridePresets',
-          overrides.filter(d => d !== id)
-        )
-      }
+    computed: {
+      presets() {
+        return this.$store.state.presets.presets
+      },
+      overridePresets() {
+        return this.$store.state.presets.overridePresets
+      },
     },
-  },
-}
-</script>
-
-<style lang="sass">
-body
-  font: normal 14px Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace
-  color: #fff
-  background-color: #000
-</style>
-
-<style lang="sass" scoped>
-.wrap
-  display: grid
-  grid-template-columns: 1fr 300px
-  margin-bottom: 200px
-
-.input-group
-  display: inline-block
-  margin: 0 5px
-  color: #444
-  font-size: 13px
-  text-align: center
-  line-height: 1.7em
-
-  .label
-    user-select: none
-
-.presets
-  position: fixed
-  bottom: 0
-  left: 0
-
-.preset-selector
-  display: inline-block
-  margin: 2px
-  padding: 2px 3px
-  font-size: 12px
-  background-color: #111
-  color: #888
-  cursor: pointer
-
-  &.is-active
-    background-color: #033
+    mounted() {
+      window.addEventListener('keydown', e => {
+        if (e.key === ' ') {
+          this.$store.dispatch('tap')
+          e.preventDefault()
+        }
+      })
+    },
+    methods: {
+      togglePreset(id) {
+        const overrides = this.$store.state.presets.overridePresets
+        if (!overrides.includes(id)) {
+          this.$store.dispatch('updateOverridePresets', [...overrides, id])
+        } else {
+          this.$store.dispatch(
+            'updateOverridePresets',
+            overrides.filter(d => d !== id)
+          )
+        }
+      },
+      addressSetUp() {
+        this.isPopupVisible = true;
+      },
+      endSetUp() {
+        this.isPopupVisible = false;
+      },
+      startDrag(event) {
+        this.dragData.x = event.clientX - this.popupPosition.x;
+        this.dragData.y = event.clientY - this.popupPosition.y;
+        document.addEventListener('mousemove', this.drag);
+        document.addEventListener('mouseup', this.stopDrag);
+      },
+      drag(event) {
+        this.popupPosition.x = event.clientX - this.dragData.x;
+        this.popupPosition.y = event.clientY - this.dragData.y;
+      },
+      stopDrag() {
+        document.removeEventListener('mousemove', this.drag);
+        document.removeEventListener('mouseup', this.stopDrag);
+      },
+    },
+  }
+  </script>
+  
+  <style lang="sass">
+  body
+    font: normal 14px Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace
     color: #fff
-</style>
+    background-color: #000
+  </style>
+  
+  <style lang="sass" scoped>
+  .popup
+    position: fixed
+    background-color: rgba(0, 0, 0, 0.5)
+    display: flex
+    justify-content: center
+    align-items: center
+  
+    .popup-content
+    background-color: white
+    color: black
+    padding: 20px
+    border-radius: 5px
+    text-align: center
+    cursor: move
+  
+  .wrap
+    display: grid
+    grid-template-columns: 1fr 300px 300px
+    margin-bottom: 200px
+  
+  .container
+    display: flex
+    flex-direction: column
+    align-items: left
+    justify-content: center
+    height: 45vh
+  
+  .input[type="text"]
+    margin-bottom: 10px
+  
+  .button
+    margin-bottom: 10px
+  
+    
+  .input-group
+    display: inline-block
+    margin: 0 5px
+    color: #444
+    font-size: 13px
+    text-align: center
+    line-height: 1.7em
+  
+    .label
+      user-select: none
+  
+  .presets
+    position: fixed
+    bottom: 0
+    left: 0
+  
+  .preset-selector
+    display: inline-block
+    margin: 2px
+    padding: 2px 3px
+    font-size: 12px
+    background-color: #111
+    color: #888
+    cursor: pointer
+  
+    &.is-active
+      background-color: #033
+      color: #fff
+  </style>
